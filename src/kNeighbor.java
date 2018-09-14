5dc1c9ec96477dee797fcacc8dfa61dc0ed8b4a3
Sahil <dhokedsahil@yahoo.co.in>
2018-09-13 22:50:30 -0500

    terminating condition

--- src/kNeighbor.java
+++ src/kNeighbor.java
@@ -7,6 +7,7 @@ import java.util.concurrent.locks.Lock;
 //This algorithm works in phases
 //First send all immediate neighbors the new nodes added in the last phase, then receive the same
 //Second send okay to mark end of phase and start new phase only when okay received from all immediate neighbors
+//Terminate messages should only be sent in okay phase
 public class kNeighbor{
 	Lock l;
 	Integer nodeid;
@@ -15,9 +16,11 @@ public class kNeighbor{
 	HashSet<Integer> neighborsDiscovered;//hashset to ensure no node is added twice
 	Integer immediateNeighbors;
 	Integer currentPhaseReceived;//variable to keep track of how many immediate neighbors contacted this node
-	Integer okayReceived;//varialbe to keep track of how many immediate neighbors have sent okay
+	Integer okayReceived;//varialbe to keep track how many immediate neighbors have sent okay
+	Integer terminateReceived;//variable to track how many immediate neighbors have terminated
 	boolean change;//variable to check if any change occurred in current phase
-	boolean toTerminate;//varaible to track end of algorithm
+	boolean toTerminate;
+	boolean terminated;//varaible to track end of algorithm
 	Broadcaster b;
 	
 	public kNeighbor(Integer nodeid, Integer neighborCount, Broadcaster broadcaster){
@@ -28,9 +31,11 @@ public class kNeighbor{
 		immediateNeighbors = neighborCount;
 		currentPhaseReceived = 0;
 		okayReceived = 0;
+		terminateReceived = 0;
 		change = false;
 		b = broadcaster;
-		toTerminate = false;
+		toTerminate = false;//varialbe to track whether algorithm needs to terminate in next okay phase
+		terminated = false;
 	}
 	
 	public void start(){
@@ -42,8 +47,12 @@ public class kNeighbor{
 	
 	//Upon receiving a new message
 	void receive(StreamMsg m){
-		if(toTerminate) return;
+		if(terminated) return;
 		l.lock();
+		if(m.type == MsgType.terminate){
+			terminateReceived++;
+			toTerminate = true;
+		}
 		if(m.type == MsgType.neighbor){				
 			for(Integer phaseNeighbor : m.phaseNeighbors){
 				//Check if neighbor is already discovered
@@ -58,28 +67,33 @@ public class kNeighbor{
 			}
 			
 			currentPhaseReceived++;
-			if(currentPhaseReceived == immediateNeighbors){
+			if(currentPhaseReceived + terminateReceived == immediateNeighbors){
 				if(!change){
 					//Terminate
-					terminate();
+					toTerminate = true;
 				}
-				System.out.println("kHopNeighbors phase "+phase+"; neighbours "+kHopNeighbors.get(phase));
-				StreamMsg okayMessage = new StreamMsg();
-				okayMessage.type = MsgType.okay;
-				send(okayMessage);
+				System.out.println("kHopNeighbors phase " + phase + "; neighbours " + kHopNeighbors.get(phase));
+				if(toTerminate){
+					sendTerminate();//Terminate message also acts like okay message. It's like okay and terminate
+				}
+				else{
+					sendOkay();					
+				}
 				phase++;
 				currentPhaseReceived = 0;//reset
 				change = false;
 			}
 		}
-		else if(m.type == MsgType.okay){
+		else if(m.type == MsgType.okay || m.type = MsgType.terminate){
 			okayReceived++;
 			if(okayReceived == immediateNeighbors){
 				StreamMsg m1 = new StreamMsg();
 				//m.phaseNo = phase;
-				m1.type = MsgType.neighbor;
-				m1.phaseNeighbors = kHopNeighbors.get(phase-1);
-				send(m1);
+				if(!terminated){
+					m1.type = MsgType.neighbor;
+					m1.phaseNeighbors = kHopNeighbors.get(phase-1);
+					send(m1);
+				}
 				okayReceived = 0;
 			}			
 		}
@@ -90,10 +104,16 @@ public class kNeighbor{
 		b.broadcast(m);
 	}
 	
-	void terminate(){
-		toTerminate = true;
+	void sendOkay(){
+		StreamMsg okayMessage = new StreamMsg();
+		okayMessage.type = MsgType.okay;
+		send(okayMessage);
+	}
+	
+	void sendTerminate(){
 		StreamMsg terminateMessage = new StreamMsg();
 		terminateMessage.type = MsgType.terminate;
-		send(m);
+		send(terminateMessage);
+		terminated = true;
 	}
 };
